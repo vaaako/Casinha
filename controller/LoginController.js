3d0640df8359c2d669a53fdd08a76a0667503a97
vaaako <matharth520@gmail.com>
2023-08-30 00:22:00 -0300

    added check for body

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -1,13 +1,11 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const { checkAndCreate } = require('../database/db');
-const { validAll, usernameIsInUse, emailIsInUse } = require('../database/validator');
+const { validAll, isUsernameValid, usernameIsInUse, emailIsInUse, errorMessages, isPasswordValid, validUsernameAndPword } = require('../database/validator');
 const { storeMessage, getMessage } = require('./MessageTool');
 const User = require('../models/User');
 
 const argon2 = require('argon2');
-
-require('dotenv').config();
 
 /** TO DO 
  * - All password requests must be encrypted
@@ -25,14 +23,6 @@ require('dotenv').config();
  * PASSWORD => LENGTH (8), EQUALS
  * EMAIL    => REGEX
  * */
-const isUsernameValid = (username) => /^[A-Za-z0-9_]+$/g.test(username) && username.length >= 4 && username.length <= 25
-const isEmailValid    = (email) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
-const isPasswordValid = (password, confirmpword) => password == confirmpword && password.length > 8;
-
-const USERNAME_ERROR_MESSAGE = "Username must be between 4 and 25 characters and can only contain Alphanumeric and _";
-const PASSWORD_ERROR_MESSAGE = "Password must has at least 8 characters!";
-const EMAIL_ERROR_MESSAGE    = "Email invalid!";
-
 
 
 // Authenticates user
@@ -40,19 +30,12 @@ const EMAIL_ERROR_MESSAGE    = "Email invalid!";
 passport.use(new LocalStrategy({ passReqToCallback: true }, async (req, username, password, done) => {
 	// Post Login run this
 	// Try to find user
-	// console.log("PASSPORT =>", username, password);
-
 	// This only will be an error if the user send manually request
-	let errorMessage = "";
-	if(!isUsernameValid(username)) // Test username 
-		errorMessage = USERNAME_ERROR_MESSAGE;
-	else if(password.length < 8) // Test password
-		errorMessage = PASSWORD_ERROR_MESSAGE;
-	// If have erroMessage (had error)
+	let errorMessage = validUsernameAndPword(username, password)
 	if(errorMessage) return done(null, false, storeMessage(errorMessage));
 
 
-	let user = await User.findOne({ username: username });
+	let user = await User.findOne({ username: username }).lean();
 
 	// User not found
 	if(!user) return done(null, false, storeMessage("User or password incorrect!"));
@@ -92,10 +75,7 @@ passport.deserializeUser((user, done) => {
 });
 
 
-const loginGET = async (req, res) => {
-	res.render('login', { message: getMessage() });
-}
-
+const loginGET = async (req, res) => res.render('login', { message: getMessage() });
 const signup = async (req, res) => res.render('signup', { message: getMessage() });
 
 
@@ -119,37 +99,42 @@ const signup = async (req, res) => res.render('signup', { message: getMessage() 
 
 // Create User
 const createUser = async (req, res) => {
-	// Get fields
-	const { username, password, confirmpword, email } = req.body;
+	try {
+		// Check body
+		if(!req.body) throw new Error("Bad request!");
 
-	// Also verify fields in sever-side
-	// This will only be invalid if the user change the HTML and JS validator
-	let errorMessage = validAll(username, email, password, confirmpword)
-		|| await usernameIsInUse(username) || await emailIsInUse(email)
+		// Get fields
+		const { username, password, confirmpword, email } = req.body;
+		
+		// Also verify fields in sever-side
+		// This will only be invalid if the user change the HTML and JS validator
+		let errorMessage = validAll(username, email, password, confirmpword)
+			|| await usernameIsInUse(username) || await emailIsInUse(email)
 
-	// If have erroMessage (had error)
-	if(errorMessage) {
-		storeMessage(errorMessage)
-		// res.status(401).send({ message: errorMessage });
+		// If have erroMessage (had error)
+		if(errorMessage) throw new Error(errorMessage);
+
+		
+		// Add entry (checking if have same ID) and removing confirmPword
+		let entry = await checkAndCreate(User, {
+			username: username,
+			nickname: username,
+			password: await argon2.hash(password, {
+				secret: Buffer.from(process.env.ARGON2_SECRET),
+				type: argon2.argon2id // Is a hybrid combination of argon2i and argonid, being resistant against GPU and tradeoff attacks
+			}),
+			email: email
+		});
+		await entry.save();
+
+		// Confirmation message to Login page
+		storeMessage("Account created with sucess!", 'sucess');
+		return res.redirect('/login');
+	} catch (err) {
+		console.log("[ERR] SIGN UP => ", err);
+		storeMessage(err); // Only happening if user send request manually
 		return res.redirect('/signup');
 	}
-
-	
-	// Add entry (checking if have same ID) and removing confirmPword
-	let entry = await checkAndCreate(User, {
-		username: username,
-		nickname: username,
-		password: await argon2.hash(password, {
-			secret: Buffer.from(process.env.ARGON2_SECRET),
-			type: argon2.argon2id // Is a hybrid combination of argon2i and argonid, being resistant against GPU and tradeoff attacks
-		}),
-		email: email
-	});
-	await entry.save();
-
-	// Confirmation message to Login page
-	storeMessage("Account created with sucess!", 'sucess');
-	return res.redirect('/login');
 }
 
 const doLogout = async (req, res, next) => {
